ee2db91dc2f7c48f95aea7c2cceb5dc738c08b2a
aquillavdberg <Aquilla.vdberg@gmail.com>
2023-01-12 16:39:24 +0100

    good sprite

--- Assets/EditorWIndowWithPopup.cs
+++ Assets/EditorWIndowWithPopup.cs
@@ -1,46 +1,46 @@
-using UnityEngine.UIElements;
-using UnityEngine;
-using UnityEditor;
-// using static EditorWindow;
+// // using UnityEngine.UIElements;
+// using UnityEngine;
+// using UnityEditor;
+// // using static EditorWindow;
 
-public class EditorWindowWithPopup : MonoBehaviour
-{
+// public class EditorWindowWithPopup : MonoBehaviour
+// {
 
-    public static GameObject popupObj;
-    // popupObj.ge
+//     public static GameObject popupObj;
+//     // popupObj.ge
 
-    // Add menu item
-    // [MenuItem("Confirm navigation")]
-    public static void Init()
-    {
-        GameObject popupObj = new GameObject("popupObj");
-        popupObj.AddComponent()
-    }
+//     // Add menu item
+//     // [MenuItem("Confirm navigation")]
+//     public static void Init()
+//     {
+//         GameObject popupObj = new GameObject("popupObj");
+//         popupObj.AddComponent()
+//     }
 
-   public static Rect buttonRect;
-    public static string OnGUI(GameObject gameObject)
-    {
-        {
-            if (gameObject.name == "No")
-                {GUILayout.Label("Are you sure you want to exit the application?", EditorStyles.boldLabel);}
-            else
-                {GUILayout.Label("Are you sure you want to go to" + gameObject.name + "?", EditorStyles.boldLabel);}
+//    public static Rect buttonRect;
+//     public static string OnGUI(GameObject gameObject)
+//     {
+//         {
+//             if (gameObject.name == "No")
+//                 {GUILayout.Label("Are you sure you want to exit the application?", EditorStyles.boldLabel);}
+//             else
+//                 {GUILayout.Label("Are you sure you want to go to" + gameObject.name + "?", EditorStyles.boldLabel);}
             
-            if (GUILayout.Button("Yes", GUILayout.Width(200)))
-                {return "true";}
-            if(GUILayout.Button("No", GUILayout.Width(200)))
-                {return "false";}
+//             if (GUILayout.Button("Yes", GUILayout.Width(200)))
+//                 {return "true";}
+//             if(GUILayout.Button("No", GUILayout.Width(200)))
+//                 {return "false";}
 
-            if (Event.current.type == EventType.Repaint) buttonRect = GUILayoutUtility.GetLastRect();
+//             if (Event.current.type == EventType.Repaint) buttonRect = GUILayoutUtility.GetLastRect();
 
-            return "still waiting";
-        }
-    }
+//             return "still waiting";
+//         }
+//     }
 
 
-    public static void Remove()
-    {
-        Destroy(popupObj);
-    }
-}
+//     public static void Remove()
+//     {
+//         Destroy(popupObj);
+//     }
+// }
 
